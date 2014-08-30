954beb2a09a425b3c31588dab4e7512da6ec1c90
happyzhangx2046 <896420643@qq.com>
2014-08-30 23:48:57 +0800

    1.2
    change the window name

--- github_test/github_test/main.cpp
+++ github_test/github_test/main.cpp
@@ -4,9 +4,9 @@ int main( int argc, char** argv )
 {
 	
 	IplImage* img123456 = cvLoadImage( argv[1] );
-		cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE );
-		cvShowImage("Example1", img123456 );
+		cvNamedWindow("Example2", CV_WINDOW_AUTOSIZE );
+		cvShowImage("Example2", img123456 );
 		cvWaitKey(0);
 		cvReleaseImage( &img123456 );
-		cvDestroyWindow("Example1");
+		cvDestroyWindow("Example2");
 }
